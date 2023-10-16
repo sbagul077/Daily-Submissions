07237ca431c968f92664719991e52e91a76ff0ba
Sanket Bagul <sbagul077@gmail.com>
2023-10-16 17:16:48 -0400

    Time: 2216 ms (77.99%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,16 +1,13 @@
 # Write your MySQL query statement below
 
 
-# select t2.unique_id, t1.name 
-# from Employees as T1 left join EmployeeUNI as T2
-# on t1.id = t2.id;
+# Left join
+select t2.unique_id, t1.name 
+from Employees as T1 left join EmployeeUNI as T2
+on t1.id = t2.id;
 
 
-# select T1.unique_id , T2.name
-# from EmployeeUNI as T1 right join Employees as T2
-# on T1.id = T2.id
-
-
-select t1.unique_id, t2.name
-from EmployeeUNI as t1 right join Employees as t2
-on t1.id = t2.id
+# Right Join
+# select t1.unique_id, t2.name
+# from EmployeeUNI as t1 right join Employees as t2
+# on t1.id = t2.id
