0db972be37e854420556ad636e5ed4a53535c126
Sanket Bagul <sbagul077@gmail.com>
2023-10-17 00:35:02 -0400

    Time: 673 ms (10.64%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,24 +1,18 @@
 # Write your MySQL query statement below
 
 
-select 
-    machine_id, 
-    round(sum(case when activity_type='start' then timestamp*-1 else timestamp end)*1.0
-    # ROUND(SUM(CASE WHEN activity_type='start' THEN timestamp*-1 ELSE timestamp END)*1.0
-      / (select count(distinct process_id)), 3) as processing_time
-from Activity
-group by machine_id;
+# select 
+#     machine_id, 
+#     round(sum(case when activity_type='start' then timestamp*-1 else timestamp end)*1.0
+#     # ROUND(SUM(CASE WHEN activity_type='start' THEN timestamp*-1 ELSE timestamp END)*1.0
+#       / (select count(distinct process_id)), 3) as processing_time
+# from Activity
+# group by machine_id;
 
-# SELECT 
-#     machine_id,
-#     ROUND(SUM(CASE WHEN activity_type='start' THEN timestamp*-1 ELSE timestamp END)*1.0
-#     / (SELECT COUNT(DISTINCT process_id)),3) AS processing_time
-# FROM 
-#     Activity
-# GROUP BY machine_id
 
-# Select a.machine_id , ROUND(avg(b.timestamp - a.timestamp),3) as processing_time
-# FROM Activity as a
-# JOIN Activity as b
-# ON a.machine_id = b.machine_id and a.process_id = b.process_id and a.activity_type = 'start' and b.activity_type = 'end'
-# GROUP BY a.machine_id;
+select t1.machine_id,
+round(avg(t2.timestamp - t1.timestamp), 3) as 'processing_time'
+from Activity as t1
+join Activity as t2
+on t1.machine_id = t2.machine_id and t1.process_id = t2.process_id and t1.timestamp < t2.timestamp
+group by t1.machine_id;
