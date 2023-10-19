9c276227d85b61666c7e5f2bf008aacdcfd3b0c7
Sanket Bagul <sbagul077@gmail.com>
2023-10-18 20:23:33 -0400

    Time: 755 ms (94.47%), Space: 0B (100.00%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -3,6 +3,6 @@
 
 select activity_date as day, count(distinct user_id) as active_users
 from Activity
-# where activity_date > '2019-06-27' AND activity_date <= '2019-07-27' 
-where DATEDIFF('2019-07-27', activity_date)<30 AND DATEDIFF('2019-07-27', activity_date)>=0 
+where activity_date > '2019-06-27' AND activity_date <= '2019-07-27' 
+# where DATEDIFF('2019-07-27', activity_date)<30 AND DATEDIFF('2019-07-27', activity_date)>=0 
 group by activity_date;
