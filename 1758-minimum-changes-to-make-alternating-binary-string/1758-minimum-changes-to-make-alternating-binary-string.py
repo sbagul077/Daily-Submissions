87f22b8ac903f14e9d9adad4b6cad09b51dae04c
Sanket Bagul <sbagul077@gmail.com>
2023-12-26 17:35:37 -0500

    Time: 37 ms (92.75%), Space: 17.5 MB (21.00%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -6,6 +6,7 @@ class Solution:
                 count +=  1 if s[i] == "0" else 0
             else:
                 count += 1 if s[i] == "1" else 0
+                
         return min(count, len(s) - count) 
         
         
