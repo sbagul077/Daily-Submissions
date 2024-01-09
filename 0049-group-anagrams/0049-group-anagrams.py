b64eabb04010fef627ceda845b72f7858b7414cc
Sanket Bagul <sbagul077@gmail.com>
2024-01-08 20:37:34 -0500

    Time: 84 ms (94.05%), Space: 20.8 MB (42.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,22 +1,23 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if strs is None or len(strs) == 0:
-            return [[""]]
-        
         hashMap = dict()
         
-        for i in range(len(strs)):
-            curr = ''.join(sorted(strs[i]))
-            # print(curr)
+        for s in strs:
+            curr = self.primeProduct(s)
+            
             if curr in hashMap.keys():
-                hashMap.get(curr).append(strs[i])
+                hashMap[curr].append(s)
             else:
-                # print(curr, hashMap)
-                hashMap[curr] = [strs[i]]
-        
+                hashMap[curr] = [s]
         
         return hashMap.values()
     
-#hashing
-#Time Complexity: O(nklogk)
-#Space Complexity: O(n)
+
+    def primeProduct(self, s):
+        result = 1
+        primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103]
+        
+        for char in s:
+            result *= primes[ord(char) - ord('a')]
+        
+        return result
