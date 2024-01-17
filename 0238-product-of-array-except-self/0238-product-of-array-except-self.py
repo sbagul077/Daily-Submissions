d9a17e3b285c649ce54e9a23db15319df3cb8b32
Sanket Bagul <sbagul077@gmail.com>
2024-01-16 20:51:00 -0500

    Time: 171 ms (75.96%), Space: 24.7 MB (32.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,29 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = [1] * len(nums)
+        n = len(nums)
+        result = [1] * n
         if nums is None or len(nums) == 0:
             return result
         
-        arr1 = [1 for i in range(len(nums))]
-        arr2 = [1 for i in range(len(nums))]
         rp = 1
-        for i in range(1, len(nums)):
-            arr1[i] = rp * nums[i - 1]
-            rp *= nums[i - 1]
+        for i in range(1, n):
+            rp = rp * nums[i - 1]
+            result[i] *= rp
         
         rp = 1
         
-        for i in range(len(nums) - 2, -1, -1):
-            arr2[i] = rp * nums[i + 1]
-            rp *= nums[i + 1]
+        for i in range(n - 2, -1, -1):
             
-        print(arr1, arr2)
-        
-        for i in range(len(arr1)):
-            result[i] = arr1[i] * arr2[i]
-        
+            rp = rp * nums[i + 1]
+            result[i] *= rp
+
+        print(result)
         return result
                 
 # Arrays
-#Time Complexity: O(2n)
-#Space Complexity: O(2n)
+#Time Complexity: O(n)
+#Space Complexity: O(1)
