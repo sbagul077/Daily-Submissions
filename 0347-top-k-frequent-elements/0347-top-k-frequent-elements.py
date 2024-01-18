395995f39b07affe7d5fb5f142f1a7ad595417ab
Sanket Bagul <sbagul077@gmail.com>
2024-01-17 20:52:04 -0500

    Time: 101 ms (39.25%), Space: 22.2 MB (21.58%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,19 +5,27 @@ class Solution:
         
         hashMap = dict()
         
-        #frequncy map
-        for num in nums:
-            hashMap[num] = hashMap.get(num, 0) + 1
-        
         pq = PriorityQueue()
         
+        for num in nums:
+            if num not in hashMap.keys():
+                hashMap[num] = 1
+            else:
+                hashMap[num] = hashMap.get(num) + 1
+        
+        # print(hashMap)
         for key in hashMap.keys():
             pq.put((hashMap.get(key), key))
+            
             if pq.qsize() > k:
                 pq.get()
         
-        for i in range(k-1, -1, -1):
+        for i in range(k - 1, -1, -1):
             curr = pq.get()
             result[i] = curr[1]
         
         return result
+
+#Dictionary, Min heap
+#Time Complexity: O(nlogk)
+#Space Complexity: O(n)
