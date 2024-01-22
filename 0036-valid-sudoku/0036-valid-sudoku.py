1814ffa405537227280299685343771c643ff9b5
Sanket Bagul <sbagul077@gmail.com>
2024-01-22 18:09:09 -0500

    Time: 81 ms (98.30%), Space: 16.6 MB (59.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -3,16 +3,27 @@ class Solution:
         if board is None or len(board) == 0:
             return False
         
-        #row 
+        #row
         for i in range(9):
             row = [False for i in range(9)]
             for j in range(9):
                 if board[i][j] != ".":
                     if row[int(board[i][j]) - 1]:
                         return False
+                    
                     row[int(board[i][j]) - 1] = True
+                    
+        #row 
+        # for i in range(9):
+        #     row = [False for i in range(9)]
+        #     for j in range(9):
+        #         if board[i][j] != ".":
+        #             if row[int(board[i][j]) - 1]:
+        #                 return False
+        #             row[int(board[i][j]) - 1] = True
         
-        #column
+        
+        #column        
         for j in range(9):
             col = [False for j in range(9)]
             for i in range(9):
@@ -20,18 +31,41 @@ class Solution:
                     if col[int(board[i][j]) - 1]:
                         return False
                     col[int(board[i][j]) - 1] = True
+        #column
+        # for j in range(9):
+        #     col = [False for j in range(9)]
+        #     for i in range(9):
+        #         if board[i][j] != ".":
+        #             if col[int(board[i][j]) - 1]:
+        #                 return False
+        #             col[int(board[i][j]) - 1] = True
         
         #block
         
         for b in range(9):
-            block = [False for b in range(9)]
-            for i in range((b // 3) * 3, ((b // 3) * 3) + 3):
-                for j in range((b % 3) * 3, (round(b % 3) * 3) + 3):
+            block = [False for i in range(9)]
+            for i in range((b//3) * 3, ((b // 3) * 3) + 3):
+                for j in range((b % 3) * 3, ((b % 3) * 3) + 3):
                     if board[i][j] != ".":
                         if block[int(board[i][j]) - 1]:
                             return False
                         block[int(board[i][j]) - 1] = True
         
         return True
+        
+        
+        
+#         #block
+        
+#         for b in range(9):
+#             block = [False for b in range(9)]
+#             for i in range((b // 3) * 3, ((b // 3) * 3) + 3):
+#                 for j in range((b % 3) * 3, (round(b % 3) * 3) + 3):
+#                     if board[i][j] != ".":
+#                         if block[int(board[i][j]) - 1]:
+#                             return False
+#                         block[int(board[i][j]) - 1] = True
+        
+#         return True
             
                     
