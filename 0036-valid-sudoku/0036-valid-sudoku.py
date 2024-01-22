d7948dd4e3eafad2741c5e71a98578802675b7e2
Sanket Bagul <sbagul077@gmail.com>
2024-01-22 18:09:58 -0500

    Time: 93 ms (73.78%), Space: 16.6 MB (60.98%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -12,16 +12,6 @@ class Solution:
                         return False
                     
                     row[int(board[i][j]) - 1] = True
-                    
-        #row 
-        # for i in range(9):
-        #     row = [False for i in range(9)]
-        #     for j in range(9):
-        #         if board[i][j] != ".":
-        #             if row[int(board[i][j]) - 1]:
-        #                 return False
-        #             row[int(board[i][j]) - 1] = True
-        
         
         #column        
         for j in range(9):
@@ -31,17 +21,8 @@ class Solution:
                     if col[int(board[i][j]) - 1]:
                         return False
                     col[int(board[i][j]) - 1] = True
-        #column
-        # for j in range(9):
-        #     col = [False for j in range(9)]
-        #     for i in range(9):
-        #         if board[i][j] != ".":
-        #             if col[int(board[i][j]) - 1]:
-        #                 return False
-        #             col[int(board[i][j]) - 1] = True
-        
+
         #block
-        
         for b in range(9):
             block = [False for i in range(9)]
             for i in range((b//3) * 3, ((b // 3) * 3) + 3):
@@ -53,19 +34,6 @@ class Solution:
         
         return True
         
-        
-        
-#         #block
-        
-#         for b in range(9):
-#             block = [False for b in range(9)]
-#             for i in range((b // 3) * 3, ((b // 3) * 3) + 3):
-#                 for j in range((b % 3) * 3, (round(b % 3) * 3) + 3):
-#                     if board[i][j] != ".":
-#                         if block[int(board[i][j]) - 1]:
-#                             return False
-#                         block[int(board[i][j]) - 1] = True
-        
-#         return True
-            
-                    
+
+#Time Complexity: O(n)
+#Space Complexity: O(1)
