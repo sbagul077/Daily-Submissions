2982fba1aef9b30fc388fb1bf2a41c15f1d58e9e
Sanket Bagul <sbagul077@gmail.com>
2024-02-20 17:54:32 -0500

    Time: 101 ms (84.05%), Space: 17.6 MB (50.08%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        low = 0
+        high = len(numbers) - 1
+        # hashMap = dict()
+        
+        while low <= high:
+            if numbers[low] + numbers[high] == target:
+                return [low + 1, high + 1]
+            elif numbers[low] +  numbers[high] > target:
+                high -= 1
+            else:
+                low += 1
+        
+        return []
+    
+#Two Pointers
+#Time Complexity: O(n)
+#Space Complexity: O(1)
