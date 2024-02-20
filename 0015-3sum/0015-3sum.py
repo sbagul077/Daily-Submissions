a9f9fb7fe8414e6a1cf91cc7c822fea5b1b877e6
Sanket Bagul <sbagul077@gmail.com>
2024-02-20 18:18:36 -0500

    Time: 572 ms (85.62%), Space: 20.8 MB (70.87%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result = list()
+        
         if nums is None or len(nums) == 0:
             return result
         
-        n = len(nums)        
         nums.sort()
         
-        for i in range(0, n):
+        for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
                 
@@ -15,33 +15,31 @@ class Solution:
                 break
             
             low = i + 1
-            high = n - 1
+            high = len(nums) - 1
             
             while low < high:
-                s = nums[i] + nums[low] + nums[high]
-                if s == 0:
+                currSum = nums[i] + nums[low] + nums[high]
+                
+                if currSum < 0:
+                    low += 1
+                  
+                elif currSum > 0:
+                    high -= 1                   
+                else:
+                    
                     li = [nums[i], nums[low], nums[high]]
                     result.append(li)
                     low += 1
-                    high -= 1
                     
                     while low < high and nums[low] == nums[low - 1]:
                         low += 1
+                    
+                    high -= 1                    
                     while low < high and nums[high] == nums[high + 1]:
                         high -= 1
-                
-                elif s < 0:
-                    low += 1
-                else:
-                    high -= 1
-        
+                    
         return result
-
-# Time Complexity: O(nlogn) + O(n)
-# Space Complexity: O(1)
-                            
-                            
-            
-                
-        
-        
+    
+# Two Pointers
+#Time Complexity: O(n^2)
+#Space Complexity: O(n)
