2fb0fecb3a26352d6a6f0592725b3e081e517c71
Sanket Bagul <sbagul077@gmail.com>
2024-03-15 22:46:01 -0400

    Time: 615 ms (68.52%), Space: 22.3 MB (33.26%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        if len(nums) == 0 or nums is None:
+            return 0
+        
+        n = len(nums)
+        maximum, count = 0, 0
+        hashMap = {0 : -1}
+        
+        for i in range(n):
+            if nums[i] == 0:
+                count -= 1
+            else:
+                count += 1
+            
+            if count in hashMap:
+                maximum = max(maximum, i - hashMap[count])
+            else:
+                hashMap[count] = i
+        
+        return maximum
