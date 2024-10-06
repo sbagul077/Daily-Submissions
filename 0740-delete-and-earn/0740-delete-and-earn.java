7da8068eb8d63d21b2d3ee3782d43f1be3206242
Sanket Bagul <sbagul077@gmail.com>
2024-10-06 01:26:42 -0400

    Time: 2 ms (99.87%), Space: 44.3 MB (66.10%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int max = 0;
+        for(int num:nums) {
+            max = Math.max(max, num);
+        }
+        int[] arr = new int[max + 1];
+        
+        for(int i = 0; i < nums.length; i++){
+            arr[nums[i]] += nums[i];
+        }
+        
+        int skip = 0;
+        int take = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            int temp = skip;
+            
+            skip = Math.max(skip, take);
+            
+            take = temp + arr[i];
+        }
+        
+        return Math.max(skip, take);
+    }
+}
