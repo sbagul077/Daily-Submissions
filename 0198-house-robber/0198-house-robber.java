c50918108c5906a9a9e0d18a6abacfa91100f65a
Sanket Bagul <sbagul077@gmail.com>
2024-10-06 00:22:53 -0400

    Time: 0 ms (100.00%), Space: 41.3 MB (9.85%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -4,18 +4,17 @@ class Solution {
             return 0;
         }
         
-        int[][] dp = new int[nums.length][2];
-        dp[0][1] = nums[0];
+        int skip = 0;
+        int take = nums[0];
         for(int i = 1; i < nums.length; i++){
+            int temp = skip; // skip to store previous skip
             // not choose
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
-                
-            //choose
-            dp[i][1] = dp[i - 1][0] +  nums[i];
+            skip = Math.max(skip , take);
+            take = temp + nums[i];
         }
         
-        System.out.println(dp[nums.length - 1]);
-        return Math.max(dp[nums.length - 1][0], dp[nums.length - 1][1]);
+
+        return Math.max(skip, take);
             
     }
 }
