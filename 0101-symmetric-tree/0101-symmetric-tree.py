ef5ae6497e37b313681bb4d62d018baf0ac1a996
Sanket Bagul <sbagul077@gmail.com>
2024-11-26 00:42:34 -0500

    Time: 0 ms (100.00%), Space: 16.7 MB (56.09%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -5,13 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-
+    # isValid = None 
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         if root is None:
-            return True       
+            return True
+        
+        # self.isValid = True
         
         return self.dfs(root.left, root.right)
-
+        # return self.isValid
                 
     def dfs(self, left, right):
         # base
@@ -19,7 +21,7 @@ class Solution:
             return True
         
         if left is None or right is None or left.val != right.val:
+            # self.isValid = False
             return False
-             
         
         return self.dfs(left.left, right.right) and self.dfs(left.right, right.left)
