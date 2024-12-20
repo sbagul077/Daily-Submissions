ec77a87039b2508578267cf0ece2eda4878e426d
Sanket Bagul <sbagul077@gmail.com>
2024-12-19 23:40:22 -0500

    Time: 9 ms (22.52%), Space: 19.2 MB (7.01%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,37 +1,42 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = dict()
-        
         indegrees = [0] * numCourses
+        adj = dict()
+                
+        for curr in prerequisites:
+            if curr[1] not in adj:
+                adj[curr[1]] = []
+            
+            adj.get(curr[1]).append(curr[0])
+            
+            indegrees[curr[0]] += 1
         
-        for num in prerequisites:
-            if num[1] not in adj.keys():
-                adj[num[1]] = []
-            
-            adj[num[1]].append(num[0])
-            
-            indegrees[num[0]] += 1
-            
-        q = deque()
+        print(indegrees, adj)
+
+        queue = deque()
         count = 0
-        
-        for i in range(numCourses):
+        for i in range(len(indegrees)):
             if indegrees[i] == 0:
-                q.append(i)
+                queue.append(i)
                 count += 1
         
-        if len(q) == 0:
-            return False
-        
-        while q:
-            curr = q.popleft()
-            li = adj.get(curr)
-            if li:
-                for node in li:
-                    indegrees[node] -= 1
+        while queue:
+            curr = queue.popleft()
+            temp = adj.get(curr)
+            if temp:
+                for i in temp:
+                    indegrees[i] -= 1
 
-                    if indegrees[node] == 0:
-                        q.append(node)
+                    if indegrees[i] == 0:
                         count += 1
+                        queue.append(i)
         
         return count == numCourses
+            
+            
+# 6
+# [[2,0],[3,0],[1,2], [1,3],[4,2],[5,4],[5,3]]            
+            
+            
+        
+        
