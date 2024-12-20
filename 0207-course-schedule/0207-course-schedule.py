6ad7418a679fe7b7748145b67a0f708d3bd3082f
Sanket Bagul <sbagul077@gmail.com>
2024-12-20 01:39:17 -0500

    Time: 7 ms (49.24%), Space: 19.1 MB (7.01%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,42 +1,40 @@
-class Solution:
+class Solution:    
+    path = [] 
+    visited, adj = [], {}
+    
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegrees = [0] * numCourses
-        adj = dict()
-                
-        for curr in prerequisites:
-            if curr[1] not in adj:
-                adj[curr[1]] = []
-            
-            adj.get(curr[1]).append(curr[0])
-            
-            indegrees[curr[0]] += 1
+        self.path = [False] * numCourses
+        self.visited = [False] * numCourses
+        self.adj = {}
         
-        print(indegrees, adj)
-
-        queue = deque()
-        count = 0
-        for i in range(len(indegrees)):
-            if indegrees[i] == 0:
-                queue.append(i)
-                count += 1
-        
-        while queue:
-            curr = queue.popleft()
-            temp = adj.get(curr)
-            if temp:
-                for i in temp:
-                    indegrees[i] -= 1
-
-                    if indegrees[i] == 0:
-                        count += 1
-                        queue.append(i)
-        
-        return count == numCourses
+        for edge in prerequisites:
+            if edge[1] not in self.adj:
+                self.adj[edge[1]] = []
             
-            
-# 6
-# [[2,0],[3,0],[1,2], [1,3],[4,2],[5,4],[5,3]]            
-            
-            
+            self.adj[edge[1]].append(edge[0])
         
+        for i in range(numCourses):
+            if not self.visited[i] and self.hasCycle(i):
+                return False
         
+        return True
+    
+    def hasCycle(self, course):
+        if self.path[course]:
+            return True
+        
+        if self.visited[course]:
+            return False
+        
+        self.visited[course] = True
+        self.path[course] = True
+        
+        children = self.adj.get(course)
+        if children:
+            for i in children:
+                if self.hasCycle(i):
+                    return True
+        self.path[course] = False
+        return False
+        
+            
