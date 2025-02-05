e271a1523f25bf8c0ff37cca49acfe8873441293
Sanket Bagul <sbagul077@gmail.com>
2025-02-04 21:58:16 -0500

    Time: 0 ms (100%), Space: 42.5 MB (44.14%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -3,16 +3,17 @@ class Solution {
         if(n == 0){
             return 1.00;
         }
-        double result = myPow(x, n /2);
 
-        if(n %2 == 0){
-            return result * result; 
-        }else{
-            if(n > 0){
-                return result * result * x;
+        double result = myPow(x, n/2);
+
+        if(n % 2 == 0){
+            return result * result;
+        }else {
+            if(n < 0){
+                return result * result * (1/x);
             }
             else{
-                return result * result * (1/x);
+                return result * result * x;
             }
         }
     }
