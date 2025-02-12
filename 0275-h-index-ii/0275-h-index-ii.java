b7ea88a657e18b50163b444b40c8a2486c10c2dc
Sanket Bagul <sbagul077@gmail.com>
2025-02-12 00:39:29 -0500

    Time: 1 ms (32.03%), Space: 47.6 MB (96.3%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,25 +1,17 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int n = citations.length;
-        int low = 0;
-        int high = n - 1;
-
-        while(low <= high){
-            int mid = low + (high - low)/ 2;
-
-            if(citations[mid] == n - mid){
-                return n - mid;
-            }
-            else if(n - mid > citations[mid]){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
+        if(citations == null || citations.length == 0){
+            return 0;
         }
-        return n - low;
+        int n = citations.length;
+
+        for(int i = 0; i < citations.length; i++){
+            int diff = n - i;
+            if(citations[i] >= diff){
+                return diff;
+            }
+        } 
+
+        return 0;
     }
-}  
-// #Binary Search 
-// #Time Complexity: O(logn)
-// #Space Complexity: O(1)
+}
