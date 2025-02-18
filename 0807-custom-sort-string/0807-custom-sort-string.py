bd6649cbaa92f3e998762897f95ae93589b29899
Sanket Bagul <sbagul077@gmail.com>
2025-02-18 18:56:02 -0500

    Time: 3 ms (14.6%), Space: 17.8 MB (60.46%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        hashMap = dict()
+        result = list()
+        for i in range(len(s)):
+            char = s[i]
+            
+            hashMap[char] = hashMap.get(char, 0) + 1
+        
+        for i in range(len(order)):
+            char = order[i]
+            if char in hashMap.keys(): 
+                count = hashMap.get(char)
+                while count > 0:
+                    result.append(char)
+                    count -= 1
+                hashMap.pop(char)
+        
+        for key in hashMap.keys():
+            count = hashMap.get(key)
+            while count > 0:
+                result.append(key)
+                count -= 1
+        
+     
+        return "".join(result)
+            
+#Time Complexity: O(m + n)
+#space Complexity: O(n). size of string s
