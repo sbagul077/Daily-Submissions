c475bf3746431f5e1d2945b3afa43a7e990aff4e
Sanket Bagul <sbagul077@gmail.com>
2025-03-05 01:16:46 -0500

    Time: 1 ms (23.23%), Space: 41.3 MB (88.55%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -5,29 +5,49 @@ class Solution {
 
         int m = haystack.length();
         int n = needle.length();
+        int i = 0; //source
+        int j = 0; //pattern
 
-        for (int i = 0; i <= m - n; i++) {
-            if (haystack.charAt(i) == needle.charAt(0)) {
-                int k = i;
-                int j = 0;
+        int[] lps = lps(needle);
 
-                while (k < m && j < n) {
-                    char hay = haystack.charAt(k);
-                    char need = needle.charAt(j);
-                    if (hay == need) {
-                        k++;
-                        j++;
-                    } else {
-                        break;
-                    }
-                    if (j == n) {
-                        return i;
-                    }
+        while(i < m){
+            if(haystack.charAt(i) == needle.charAt(j)){
+                i += 1;
+                j += 1;
+               
+                if(j == n){
+                    return i - n;
                 }
             }
-
+            else if(haystack.charAt(i) != needle.charAt(j) && j > 0){
+                j = lps[j - 1];
+            }else if(haystack.charAt(i) != needle.charAt(j) && j == 0){
+                i++;
+            }
         }
 
         return -1;
+    }
+
+    private int[] lps(String needle){
+        int[] lps = new int[needle.length()];
+        lps[0] = 0;
+        int i = 1;
+        int j = 0;
+
+        while(i < needle.length()){
+            if(needle.charAt(i) == needle.charAt(j)){
+                j +=1;
+                lps[i] = j;
+                i++;
+            }
+            else if(needle.charAt(i) != needle.charAt(j) && j > 0){
+                j = lps[j - 1];
+            }else if(needle.charAt(i) != needle.charAt(j) && j == 0){
+                lps[i] = 0;
+                i++;
+            }
+        }
+        return lps;
     }
 }
