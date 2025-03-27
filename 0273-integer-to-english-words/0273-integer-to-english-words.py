90bab7561852cb465e52f570a3ca9d5b437dbb48
Sanket Bagul <sbagul077@gmail.com>
2025-03-26 22:43:10 -0400

    Time: 0 ms (100%), Space: 18 MB (44.08%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.py
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -1,40 +1,40 @@
 class Solution:
     tens = []
     below_20 = []
-    def numberToWords(self, num: int) -> str:       
-        
-        self.tens = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
-        
-        self.below_20 = ["Zero","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
-        
-        if num == 0:
-            return self.below_20[0]
-        
-        thousands = [" ", "Thousand ", "Million ", "Billion ", "Trillion "]
-        
-        i = 0
+    # result = []
+    def numberToWords(self, num: int) -> str:
+        self.tens = ["","Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty","Seventy", "Eighty", "Ninety"]
+        self.below_20 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", 
+        "Thirteen", "Fourteen","Fifteen", "Sixteen", "Seventeen","Eighteen", "Nineteen"]
         result = ""
-        
+        i = 0
+        thousands = [" ", "Thousand ", "Million ", "Billion ", "Trillion "]
+
+        if num == 0:
+            return "Zero"
+
         while num > 0:
-            if num % 1000 != 0:         
-                result =  self.helper(num % 1000) + thousands[i] + result            
+            # num = num // 1000
+            if num % 1000 > 0:
+                result = self.helper(num % 1000) + thousands[i] + result
+            i += 1
             num = num // 1000
-            i += 1        
+        
         return result.strip()
     
-    
     def helper(self, num):
+
         if num == 0:
             return ""
         
         if num < 20:
             return self.below_20[num] + " "
-        
+
         elif num < 100:
+            # print(num // 10, num % 10)
             return self.tens[num // 10] + " " + self.helper(num % 10)
         else:
-            return self.below_20[num //100] + " Hundred " + self.helper(num % 100)
+            # print(num // 100, num % 100)
+            return self.below_20[num // 100] + " Hundred " + self.helper(num % 100)
 
-# Recursion, Coding
-# Time Complexity: O(1)
-# Space Complexity: O(1)
+        
