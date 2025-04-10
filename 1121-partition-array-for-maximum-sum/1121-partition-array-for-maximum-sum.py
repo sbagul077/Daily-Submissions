4bd9a48ee745d2eafda66d7acb27a085a363cde4
Sanket Bagul <sbagul077@gmail.com>
2025-04-10 00:27:39 -0400

    Time: 325 ms (44.81%), Space: 17.8 MB (69.11%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
@@ -2,22 +2,24 @@ class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         if arr is None or len(arr) == 0:
             return 0
-        
+
         size = len(arr)
-        dp = [0] * size
+        dp = [0 for i in range(size)]
         dp[0] = arr[0]
-        
-        for i in range(1, size):
-            self.max = dp[i]
+
+        for i in range(1, size): 
+            maximum = arr[i]
             for j in range(1, k + 1):
                 if i - j + 1 >= 0:
-                    self.max = max(self.max, arr[i - j + 1]) #max between number of elements in the partitions window
-                    if i - j >= 0: #check if dp[i] is not negative
-                        dp[i] = max(dp[i], (self.max * j + dp[i - j]))
+                    maximum = max(maximum, arr[i - j + 1])
+                    if i - j >= 0:
+                        dp[i] = max(dp[i], maximum * j + dp[i - j])
                     else:
-                        dp[i] = self.max * j
+                        dp[i] = max(dp[i], maximum * j )
+                j -= 1
+        
         return dp[-1]
-                        
-#Bottom Up DP
-#Time Complexity: O(n * k)
-#Space Complexity: O(n)
+
+# //DP
+# //Time Complexity: O(n * k)
+# //Space Complexity: O(n)
