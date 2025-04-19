4aa3ba6efd850fce0879d0c836a83a3e4bc93438
Sanket Bagul <sbagul077@gmail.com>
2025-04-19 00:07:11 -0400

    Time: 318 ms (48.63%), Space: 18 MB (43.5%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
@@ -2,12 +2,13 @@ class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         if arr is None or len(arr) == 0:
             return 0
-
+        
         size = len(arr)
+
         dp = [0 for i in range(size)]
         dp[0] = arr[0]
 
-        for i in range(1, size): 
+        for i in range(1, size):
             maximum = arr[i]
             for j in range(1, k + 1):
                 if i - j + 1 >= 0:
@@ -15,11 +16,7 @@ class Solution:
                     if i - j >= 0:
                         dp[i] = max(dp[i], maximum * j + dp[i - j])
                     else:
-                        dp[i] = max(dp[i], maximum * j )
+                        dp[i] = max(dp[i], maximum * j)
                 j -= 1
         
         return dp[-1]
-
-# //DP
-# //Time Complexity: O(n * k)
-# //Space Complexity: O(n)
