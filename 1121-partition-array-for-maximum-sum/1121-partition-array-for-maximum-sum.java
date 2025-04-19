af304fc82b213f41c02eb0b6f53df6deddf27a0d
Sanket Bagul <sbagul077@gmail.com>
2025-04-19 00:34:22 -0400

    Time: 6 ms (94.18%), Space: 42.1 MB (79.22%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
@@ -1,29 +1,27 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        if (arr == null || arr.length == 0)
-            return 0;
+        if(arr ==null || arr.length == 0) return 0;
 
         int size = arr.length;
         int[] dp = new int[size];
         dp[0] = arr[0];
 
-        for (int i = 1; i < size; i++) {
+        for(int i = 1; i < size; i++){
             int max = arr[i];
-            for (int j = 1; j <= k && i - j + 1 >= 0; j++) {
-                max = Math.max(max, arr[i - j + 1]);
-                if (i - j >= 0) {
-                    dp[i] = Math.max(dp[i],max * j + dp[i - j]);
-                }else{
-                    dp[i] = Math.max(dp[i],max * j);
+            for(int j = 1; j <= k; j++){
+                if(i - j + 1 >= 0){
+                    if(max < arr[i - j + 1]){
+                        max = arr[i - j + 1];
+                    }
+                    if(i - j >= 0){
+                        dp[i] = Math.max(dp[i], max * j + dp[i - j]);
+                    }
+                    else{
+                        dp[i] = Math.max(dp[i], max * j);
+                    }
                 }
             }
         }
-
         return dp[dp.length - 1];
-
     }
 }
-
-//DP
-//Time Complexity: O(n * k)
-//Space Complexity: O(n)
