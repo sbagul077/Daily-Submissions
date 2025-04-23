bf04dddf2eb73acff6c2f8b319669733e0b90886
Sanket Bagul <sbagul077@gmail.com>
2025-04-23 15:57:57 -0400

    Time: 7 ms (80.09%), Space: 18 MB (97.93%) - LeetHub

---
+++ 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        hashMap = defaultdict(list)
+
+        for (t, user, web) in sorted(zip(timestamp, username, website)):
+            hashMap[user].append(web)
+
+        scores = defaultdict(int)
+
+        for user, website in hashMap.items():
+            for pattern in set(combinations(website, 3)):                
+                scores[pattern] += 1
+        
+        maxPattern = ""
+        maxCount = 0
+
+        for pattern, cnt in scores.items():
+            if cnt > maxCount or (cnt == maxCount and pattern < maxPattern):
+                maxPattern = pattern
+                maxCount = cnt
+        
+        return maxPattern
