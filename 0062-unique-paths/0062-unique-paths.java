2585e36beb229e4558f4da363abbe92e5c97bfb4
Sanket Bagul <sbagul077@gmail.com>
2025-04-29 18:15:23 -0400

    Time: 0 ms (100%), Space: 40.1 MB (95.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,27 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        if(m==0 && n==0) return 0;
+
         int[][] dp = new int[m + 1][n + 1];
 
-        dp[m - 1][n - 1] = 1;
+        dp[1][1] = 1;
 
-        for(int i = m- 1; i >= 0; i--){
-            for(int j = n - 1; j>= 0; j--){
-                if(i == m- 1 && j == n - 1){
+        for(int i = 1; i <= m; i++){
+            for(int j = 1;j <= n; j++){
+                if( i== 1 && j == 1){
                     continue;
                 }
 
-                dp[i][j] = dp[i + 1][j] + dp[i][j+ 1];
+                dp[i][j] = dp[i - 1][j] + dp[i][j -1];
             }
         }
-        return dp[0][0];
+
+        return dp[m][n];
+
+        
     }
 }
+
+// #bottom up DP
+// #Time Complexity:O(m*n)
+// #Space Complexity: O(m*n)
