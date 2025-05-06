e15f0a51d9e9f65d3bc0af92d75577db610d156f
Sanket Bagul <sbagul077@gmail.com>
2025-05-06 11:36:45 -0400

    Time: 3 ms (64.96%), Space: 54.1 MB (34.52%) - LeetHub

---
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] indegrees = new int[n];
+
+        for(int[] t: trust){
+            indegrees[t[0] - 1] -= 1;
+            indegrees[t[1] - 1] += 1;
+        }
+
+        for(int i = 0; i < indegrees.length; i++){
+            if(indegrees[i] == n - 1){
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+}
+
+
+// Graph 
+// Topological sort
+// Time Complexity: O(V+E)
+// Space Complexity: O(V)
