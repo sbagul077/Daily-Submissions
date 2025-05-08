4ae9c43dd60c8e7dc7a99e384bc94873f3227eba
Sanket Bagul <sbagul077@gmail.com>
2025-05-08 16:15:32 -0400

    Time: 66 ms (5.94%), Space: 13.9 MB (100%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -8,35 +8,27 @@ class Solution:
         if l1 is None or l2 is None:
             return 0
         
-        count = list()
+        dummy = ListNode()
+        temp = dummy
+        currSum = 0
         
-        while l1:
-            count.append(str(l1.val))
-            l1 = l1.next
+        while (l1 != None or l2 != None) or currSum:
+            total = 0
+            if l1 != None:
+                total += l1.val
+                l1 = l1.next
+            if l2 != None:
+                total += l2.val
+                l2 = l2.next
             
-        count = count[::-1]
+            total += currSum
+            currSum = total // 10
+            node = ListNode(total % 10)
+            temp.next = node
+            temp = temp.next
         
-        count1 = list()
-        
-        while l2:
-            count1.append(str(l2.val))
-            l2 = l2.next
-            
-        count1 = count1[::-1]    
-        
-        count = int("".join(count))
-        count1 = int("".join(count1))
-        count += count1
-        result = list()
-        count = str(count)
+        return dummy.next
 
-        head = ListNode(None)
-        curr = head
-        for i in range(len(count)- 1, -1, -1):
-            # print(count[i])
-            head.next = ListNode(int(count[i]))
-            head = head.next
-            
-            
-        return curr.next
-        
+#maths
+#Time Complexity: O(m + n)
+#Space Complexity :O(1)
