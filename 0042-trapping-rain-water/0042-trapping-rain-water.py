00a6580033f10f2e598b7db6e8e974d37e0e0293
Sanket Bagul <sbagul077@gmail.com>
2025-05-09 22:55:08 -0400

    Time: 2 ms (98.43%), Space: 19.3 MB (69.8%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,28 +1,29 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        result = 0 
         if height is None or len(height) == 0:
-            return result
+            return 0
         
-        left, right = 0, len(height) - 1
-        lw, rw = 0, 0
-        
+        left = 0
+        right = len(height) - 1
+        lw = 0
+        rw = 0
+        result = 0
+
         while left <= right:
             if lw <= rw:
-                if height[left] < lw:
+                if lw > height[left]:
                     result += lw - height[left]
+                    left += 1
                 else:
                     lw = height[left]
-                left += 1
+                    left += 1
             else:
-                if height[right] < rw:
+                if rw > height[right]:
                     result += rw - height[right]
+                    right -= 1
                 else:
                     rw = height[right]
-                right -= 1
+                    right -= 1
         
         return result
 
-#Two Pointers
-#Time Complexity: O(n)
-#Space Complexity: O(1
