b3c7125d70548b0236f88ed89307595148421800
Sanket Bagul <sbagul077@gmail.com>
2025-07-07 08:46:32 -0400

    Time: 4 ms (59.65%), Space: 25.6 MB (75.19%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -8,19 +8,19 @@ class Solution:
         
         n = len(nums)
 
-
         if k >= n:
             k = k % n
         
-        self.reverse(nums, 0, n- 1)
+        self.reverse(nums, 0, n-1)
         self.reverse(nums, 0, k - 1)
-        self.reverse(nums, k, n - 1)
+        self.reverse(nums, k, n- 1)
 
-
+        # return nums
+    
     def reverse(self, nums, left, right):
-        while left < right:
+
+        while left <= right:
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
             right -= 1
-        
         
