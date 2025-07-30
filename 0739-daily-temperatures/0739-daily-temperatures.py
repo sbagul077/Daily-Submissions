fa667b398c3dd1e1690daf8315923673fdf6c327
Sanket Bagul <sbagul077@gmail.com>
2025-07-30 11:14:15 -0400

    Time: 79 ms (95.25%), Space: 27.1 MB (80.76%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,17 +1,19 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        if temperatures is None or len(temperatures) == 0:
-            return [0]
-
         n = len(temperatures)
-        result = [0 for i in range(n)]
+        result = [0] * n
+
+        if temperatures is None or len(temperatures) == 0:
+            return result
+        
         st = []
 
-        for index in range(n):
-            while len(st) > 0 and temperatures[index] > temperatures[st[-1]]:
-                top = st.pop(-1)
+        for index in range(len(temperatures)):
+            while st and temperatures[index] > temperatures[st[-1]]:
+                top = st.pop()
                 result[top] = index - top
-            
             st.append(index)
-        
         return result
+    
+#Time complexity: O(n)
+#Space Complexity: O(n)
