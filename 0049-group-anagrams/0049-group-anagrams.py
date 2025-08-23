5b9167ddc47bad9e5dd2a88404ab11ae55856291
Sanket Bagul <sbagul077@gmail.com>
2025-08-23 09:15:47 -0400

    Time: 12 ms (59.81%), Space: 20.7 MB (76.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,23 +1,28 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if strs is None or len(strs) == 0:
+            return []
+        
         hashMap = dict()
-        
-        for s in strs:
-            curr = self.primeProduct(s)
-            
-            if curr in hashMap.keys():
-                hashMap[curr].append(s)
-            else:
-                hashMap[curr] = [s]
-        
-        return hashMap.values()
-    
 
-    def primeProduct(self, s):
+        for str in strs:
+            primeProduct = self.getPrimeProduct(str)
+
+            if(primeProduct not in hashMap.keys()):
+                hashMap[primeProduct] = list()
+            
+            hashMap.get(primeProduct).append(str)
+        # print(hashMap)
+        return list(hashMap.values())
+    
+    def getPrimeProduct(self, word):
+
         result = 1
-        primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103]
+
+        primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
+                  103]
         
-        for char in s:
-            result *= primes[ord(char) - ord('a')]
-        
+        for char in word:
+            result = result * primes[ord(char) - ord("a")]
+
         return result
