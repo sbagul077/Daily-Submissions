aa770f7082b007474a634d58f8e761b8cefa9c69
Sanket Bagul <sbagul077@gmail.com>
2025-09-15 04:58:10 -0400

    Time: 40 ms (61.41%), Space: 30 MB (24.28%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.py
+++ 0816-design-hashset/0816-design-hashset.py
@@ -1,49 +1,54 @@
 class MyHashSet:
 
-    def __init__(self):
-        self.buckets = 1000
-        self.bucketItems = 1000
-        self.storage = [False for i in range(self.buckets)]   
- 
-    def getbucket(self, key):
-        return key % self.buckets
-    
-    def getbucketItem(self, key):
-        return key // self.bucketItems 
-    
-    def add(self, key: int) -> None:
-        bucket = self.getbucket(key)
-        bucketItem = self.getbucketItem(key)
+    bucket = 0;
+    bucketItems = 0;
+    storage = []
 
-        if self.storage[bucket] == False:
-            self.storage[bucket] = [False] * 1000
+    def __init__(self):
+        self.bucket = 1000;
+        self.bucketItems = 1000;
+        self.storage = [None] * self.bucket
+
+    def getBucket(self, key):
+        return key % 1000;
+    
+    def getBucketItems(self, key):
+        return key // 1000;
+
+    def add(self, key: int) -> None:
+        bucket = self.getBucket(key)
+        bucketItem = self.getBucketItems(key)
+        # print(bucket,self.storage)
+        if(self.storage[bucket] is None):
             if bucket == 0:
-                # self.storage[bucket] = [False] * (self.bucketItems + 1)                
-                self.storage[bucket].append(False)
-            # else:
-            #     self.storage[bucket] = []*self.bucketItems
-        # print(bucketItem, bucket)        
-        self.storage[bucket][bucketItem] =  True
-                   
+                self.storage[bucket] = [False] * (self.bucketItems + 1)
+            else:
+                self.storage[bucket] = [False] * self.bucketItems
+
+        self.storage[bucket][bucketItem] = True               
 
     def remove(self, key: int) -> None:
-        bucket = self.getbucket(key)
-        bucketItem = self.getbucketItem(key)
         
-        if self.storage[bucket] == False:
-            return
-        
-        self.storage[bucket][bucketItem] = False
+        bucket = self.getBucket(key)
+        bucketItem = self.getBucketItems(key)
+
+        if(self.storage[bucket] is not None):
+            self.storage[bucket][bucketItem] = False        
 
     def contains(self, key: int) -> bool:
-        bucket = self.getbucket(key)
-        bucketItem = self.getbucketItem(key)
+
+        bucket = self.getBucket(key)
+        bucketItem = self.getBucketItems(key)
+
+        if(self.storage[bucket]):
+            # print(bucketItem,self.storage[bucket])
+            if(self.storage[bucket][bucketItem]):
+                return True
         
-        if self.storage[bucket] == False:
-            return False
+        return False
+
+
         
-        return self.storage[bucket][bucketItem]
-    
 
 
 # Your MyHashSet object will be instantiated and called as such:
