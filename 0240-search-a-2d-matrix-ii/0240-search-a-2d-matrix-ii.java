efca3745a8470d8a0bdb1ce9fb397bbe07a2ed9e
Sanket Bagul <sbagul077@gmail.com>
2026-01-08 21:47:30 +0530

    Time: 3 ms (86.36%), Space: 48.3 MB (46.12%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,32 +1,31 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        
+        if(matrix == null || matrix.length == 0){
+            return false;
+        }
+
         int m = matrix.length;
         int n = matrix[0].length;
-        int i = m - 1;
-        int j = 0;
 
-        while(i >= 0 && j < n){
+        int i = 0;
+        int j = n - 1;
 
-            int curr = matrix[i][j];
-            System.out.print(curr);
-            if(curr == target){
+        while(i < m && j >= 0){
+            if(matrix[i][j] == target){
                 return true;
             }
-            else if(curr > target){
-                i--;
+            else if (matrix[i][j] > target){
+                j--;
             }
             else{
-                j++;
+                i++;
             }
         }
-        
-        return false;    
-        
+
+        return false;
     }
 }
 
-
-//Two Pointers
-//TC: O(m) + O(n)
-//SC: O(1)
+// two pointers 
+// time complexity: O(n)
+// space complexity: O(1)
