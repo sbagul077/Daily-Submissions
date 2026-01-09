5b744b1e65e2cfa9c817473b5205f9fdfbc63e9f
Sanket Bagul <sbagul077@gmail.com>
2026-01-09 20:32:37 +0530

    Time: 144 ms (53.54%), Space: 25.5 MB (15.84%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,20 +1,40 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        i = len(matrix) - 1
-        j = 0
-        
-        while i >= 0 and j <= len(matrix[0]) - 1:
-            curr = matrix[i][j]
-            
-            if curr == target:
-                return True
-            elif curr > target:
-                i -= 1
-            elif curr < target:
-                j += 1
+    def binarySearch(self, matrix, target, start, vertical):
+        low = start
+        high = len(matrix[0]) - 1 if vertical else len(matrix) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            if vertical:  # searching column
+                if matrix[start][mid] < target:
+                    low = mid + 1
+                elif matrix[start][mid] > target:
+                    high = mid - 1
+                else:
+                    return True
+            else:   #searching row
+                if matrix[mid][start] < target:
+                    low = mid + 1
+                elif matrix[mid][start] > target:
+                    high = mid - 1
+                else:
+                    return True
         
         return False
 
-#Two Pointers
-# TC: O(m) + O(n)
-# SC: O(1)
+
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        
+        for i in range(min(len(matrix), len(matrix[0]))):
+            vertical_found = self.binarySearch(matrix, target, i, True)
+
+            horizontal_found = self.binarySearch(matrix, target, i, False)
+
+            if vertical_found or horizontal_found:
+                return True
+        
+        return False
