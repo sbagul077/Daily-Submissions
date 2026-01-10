33ca68f8802242f5c20af77d986ce2fe7fd3bc8d
Sanket Bagul <sbagul077@gmail.com>
2026-01-10 23:32:15 +0530

    Time: 5 ms (59.35%), Space: 20.4 MB (11.3%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -3,23 +3,27 @@ class Solution:
         result = 0
         slow = 0
         n = len(nums)
-        nums.sort()
         fast = 1
+        nums.sort()
+
         while fast < n:
             curr = nums[fast] - nums[slow]
+
             if curr > k:
                 slow += 1
+            
             elif curr < k or fast == slow:
                 fast += 1
             else:
                 result += 1
                 slow += 1
                 fast += 1
-                
+
                 while fast < n and nums[fast] == nums[fast - 1]:
                     fast += 1
-
+        
         return result
-                    
-            
-            
+    
+#two pointers
+# time complexity: O(nlogn)
+# space complexity: O(1)
