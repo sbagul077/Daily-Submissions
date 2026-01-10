6680352e7d3ade371d1a41b39be6974fa0a43873
Sanket Bagul <sbagul077@gmail.com>
2026-01-10 23:40:53 +0530

    Time: 10 ms (93.07%), Space: 46.8 MB (28.73%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -1,29 +1,36 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        HashSet<List<Integer>> result = new HashSet<>();
-        
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], i);
+        int slow = 0;
+        int fast = 1;
+        int n = nums.length;
+        Arrays.sort(nums);
+        int result = 0;
+
+        while(fast < n){
+            int curr = nums[fast] - nums[slow];
+
+            if(curr > k){
+                slow += 1;
+            }
+            else if (curr < k || fast == slow){
+                fast += 1;
+            }
+            else{
+                result += 1;
+                slow++;
+                fast++;
+
+                while(fast < n && nums[fast] == nums[fast - 1]){
+                    fast += 1;
+                }
+            }
         }
 
-        for(int i = 0; i < nums.length; i++){
-            int complement = nums[i] - k;
-            if(map.containsKey(complement) && map.get(complement) != i){
-                Integer[] temp = {nums[i], complement};
-                Arrays.sort(temp);
-                result.add(Arrays.asList(temp));
-            }
-
-            complement = nums[i] + k;
-            if(map.containsKey(complement) && map.get(complement) != i){
-                Integer[] temp = {nums[i], complement};
-                Arrays.sort(temp);
-                result.add(Arrays.asList(temp));
-            }
-        }
-
-        return result.size();
+        return result;
+        
     }
 }
+
+// #two pointers
+// # time complexity: O(nlogn)
+// # space complexity: O(1)
