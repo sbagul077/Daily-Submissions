2d1973903011af64a6b4516dece1c494a10339de
Sanket Bagul <sbagul077@gmail.com>
2026-01-13 23:17:21 +0530

    Time: 1 ms (96.93%), Space: 46.5 MB (42.25%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int idx;
+    HashMap<Integer, Integer> map;
+    
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.map = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++){
+            map.put(inorder[i], i);
+        }
+              
+        return helper(preorder, 0, preorder.length - 1);
+    }
+    
+    private TreeNode helper(int[] preorder, int start, int end){
+        if(start > end){
+            return null;
+        }
+            
+        int rootVal = preorder[idx];
+        TreeNode root = new TreeNode(rootVal);
+        idx += 1;
+        int rootIdx = map.get(rootVal);
+
+        root.left = helper(preorder, start, rootIdx - 1);
+        root.right = helper(preorder, rootIdx + 1, end);
+
+        return root;               
+        }
+}
+
+// Hashing
+// TC: O(n)
+// SC: O(n)
