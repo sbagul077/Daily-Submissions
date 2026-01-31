ed249b2fd16ba32247c84fc5c7f27ef03a4bf362
Sanket Bagul <sbagul077@gmail.com>
2026-01-31 13:43:44 +0530

    Time: 0 ms (100%), Space: 43.3 MB (91.17%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -18,33 +18,24 @@ class Solution {
         if(root == null){
             return true;
         }
-        
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root.left);
-        q.add(root.right);
-        
-        while(!q.isEmpty()){
-            TreeNode left = q.poll();
-            TreeNode right = q.poll();
-            
-            if(left == null && right == null){
-                continue;
-            }
-            
-            if(left == null || right == null || left.val != right.val){
-                return false;
-            }
-            
-            q.add(left.left);
-            q.add(right.right);
-            q.add(left.right);
-            q.add(right.left);
+
+        return dfs(root.left, root.right);
+    }
+
+    private boolean dfs(TreeNode left, TreeNode right){
+        //base
+        if(left == null && right == null){
+            return true;
         }
-        
-        return true;
+
+        if(left == null || right == null || left.val != right.val){
+            return false;
+        }
+
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
     }
 }
 
-// BFS
-// Time Complexity: O(n)
-// Space Complecity: O(n)
+// # DFS
+// # Time Complexity: O(n)
+// # @Space Complexity: O(n)
