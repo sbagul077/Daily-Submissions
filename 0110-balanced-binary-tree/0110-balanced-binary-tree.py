c06f6c76220559be94a3253fcdc9f2e02575e9f0
Sanket Bagul <sbagul077@gmail.com>
2026-02-07 17:01:18 +0530

    Time: 357 ms (5.42%), Space: 20.4 MB (51.05%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -9,29 +9,19 @@ class Solution:
         if root is None:
             return True
         
-        if self.height(root) != -1:
-            return True
-        
-        return False
-    
+        return self.height(root) != -1
+
     def height(self, root):
-        #base
         if root is None:
             return 0
         
         left = self.height(root.left)
         right = self.height(root.right)
 
-
         if abs(left - right) > 1:
             return -1
         
         if left == -1 or right == -1:
             return -1
-
-        return max(left, right) + 1
         
-
-#dfs
-#time complexity: O(n)
-#space complexity: O(h)
+        return max(self.height(root.left), self.height(root.right)) + 1
