bf8bf89450e1bd04f2581866cffb6dee9354466b
Sanket Bagul <sbagul077@gmail.com>
2026-02-11 00:21:04 +0530

    Time: 6 ms (75.6%), Space: 47.3 MB (48.24%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,47 +1,72 @@
 class Solution {
-    boolean[] path;
-    boolean[] visited;
-    HashMap<Integer, List<Integer>> map;
-    
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
-        path = new boolean[numCourses];
-        visited = new boolean[numCourses];
-        map = new HashMap<>();
-        
-        for(int[] edge: prerequisites){
-            if(!map.containsKey(edge[1])){
-                map.put(edge[1], new ArrayList<>());
-            }
-            
-            map.get(edge[1]).add(edge[0]);
-        }
-        
-        
-        for(int i = 0; i < numCourses; i++){
-            if(!visited[i] && dfs(i)){
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
-    private boolean dfs(int course){
-        if(path[course]) return true;
-        
-        if(visited[course]) return false;
-        
-        visited[course] = true;
-        path[course] = true;
-        
-        if(map.get(course) != null){
-            for(int n: map.get(course)){
-                if( dfs(n)) return true;
-            }
-        }
-        // backtrack
-        path[course] = false;
-        return false;
-    }
+
+   HashMap<Integer, List<Integer>> map;
+
+   boolean [] visited;    boolean [] path; //this path is curr path that we are making starting from a node
+
+   public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+       visited = new boolean [numCourses];
+
+       path = new boolean [numCourses];
+
+       map = new HashMap<>();
+
+       for(int [] edge : prerequisites){
+
+           if(!map.containsKey(edge[1])){
+
+               map.put(edge[1], new ArrayList<>());
+
+           }
+
+           map.get(edge[1]).add(edge[0]);
+
+       }
+
+       for (int i = 0; i < numCourses; i++) {
+
+           if (!visited[i] && dfs(i)) {
+
+               return false;
+
+           }
+
+       }
+
+       return true;
+
+   }
+
+   //dfs is done for detecting cycle;
+
+   private boolean dfs(int i){
+
+       // base
+
+       if(path[i]) return true;
+
+       if(visited[i]) return false;        //logic
+
+       visited[i] = true;        //action add to current path
+
+       path[i] = true;        //recurse on all neigbours
+
+       if(map.get(i) != null){
+
+           for (int n : map.get(i)) {
+
+               if (dfs(n)) return true;
+
+           }
+
+       }
+
+       //backtrack
+
+       path[i] = false;
+
+       return false;
+
+   }
 }
