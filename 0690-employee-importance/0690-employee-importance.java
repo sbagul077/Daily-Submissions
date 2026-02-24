1c633f741e3d452dbf7deff259667eca66fa46bb
Sanket Bagul <sbagul077@gmail.com>
2026-02-25 01:16:58 +0530

    Time: 3 ms (55.34%), Space: 48.1 MB (40.41%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.java
+++ 0690-employee-importance/0690-employee-importance.java
@@ -10,26 +10,31 @@ class Employee {
 class Solution {
     public int getImportance(List<Employee> employees, int id) {
         int result = 0;
+
         HashMap<Integer, Employee> map = new HashMap<>();
 
-        for(Employee emp : employees){
+        for(Employee emp: employees){
             map.put(emp.id, emp);
         }
 
         Queue<Integer> q = new LinkedList<>();
         q.add(id);
 
+
         while(!q.isEmpty()){
-            int curr = q.poll();
-            Employee emp = map.get(curr);
+            Employee currEmp = map.get(q.poll());
 
-            result += emp.importance;
+            result += currEmp.importance;
 
-            for(int sub : emp.subordinates){
-                q.add(sub);
+            for(int i = 0; i< currEmp.subordinates.size(); i++){
+                q.add(currEmp.subordinates.get(i));
             }
         }
 
         return result;
     }
 }
+
+//BFS
+// Time Complexity: O(N)
+// Space Complexity: O(N)
