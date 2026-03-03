de5cf54e3b66ffee279867551731348bdb2b0d0f
Sanket Bagul <sbagul077@gmail.com>
2026-03-04 00:04:13 +0530

    Time: 3 ms (86.86%), Space: 52.6 MB (32.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,43 +1,48 @@
 class Solution {
+    int m;
+    int n;
+    int[][] dirs;
+    int count;
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0){
+        m = grid.length;
+        n = grid[0].length;
+        if(grid == null || m == 0){
             return 0;
         }
 
-        int m = grid.length;
-        int n = grid[0].length;
-
-        int[][] dirs = new int[][]{{0, 1}, {0, -1},{1, 0},{-1, 0}};
-        Queue<int[]> q = new LinkedList<>();
-        int count = 0;
+        dirs = new int[][]{{0, 1}, {0, -1},{1, 0},{-1, 0}};
+        count = 0;
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
                     count++;
-                    grid[i][j] = '0';
-                    q.add(new int[]{i, j});
-                    while(!q.isEmpty()){
-                        int[] curr = q.poll();
-
-                        for(int[] dir: dirs){
-                            int nr = dir[0] + curr[0];
-                            int nc = dir[1] + curr[1];
-
-                            if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == '1'){
-                                grid[nr][nc] = '0';
-                                q.add(new int[]{nr, nc});
-                                
-                            }
-                        }
-                    }
+                    dfs(grid, i, j);
                 }
+
             }
         }
+
         return count;
+    }
+
+    private void dfs(char[][] grid, int row, int col){
+        //base
+        if(row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == '0'){
+            return;
+        }
+        //logic
+        grid[row][col] = '0';
+
+        for(int[] dir: dirs){
+            int nr = dir[0] + row;
+            int nc = dir[1] + col;
+
+            dfs(grid, nr, nc);
+        }
     }
 }
 
-//BFS 
+//DFS 
 // Time Complexity: O(2(M *N))
 // Space Complexity: O(Min(M,N))
