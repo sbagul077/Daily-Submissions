1869bf19e2f0fdbf1d4c3129df296738b65906a5
Sanket Bagul <sbagul077@gmail.com>
2026-03-03 23:54:57 +0530

    Time: 8 ms (11.07%), Space: 51.2 MB (97.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -4,37 +4,40 @@ class Solution {
             return 0;
         }
 
-        int result = 0;
-        Queue<int []> q = new LinkedList<>();
         int m = grid.length;
         int n = grid[0].length;
+
+        int[][] dirs = new int[][]{{0, 1}, {0, -1},{1, 0},{-1, 0}};
+        Queue<int[]> q = new LinkedList<>();
+        int count = 0;
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
-                    dfs(grid, i, j, m, n);
-                    result += 1;
+                    count++;
+                    grid[i][j] = '0';
+                    q.add(new int[]{i, j});
+                    while(!q.isEmpty()){
+                        int[] curr = q.poll();
+
+                        for(int[] dir: dirs){
+                            int nr = dir[0] + curr[0];
+                            int nc = dir[1] + curr[1];
+
+                            if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == '1'){
+                                grid[nr][nc] = '0';
+                                q.add(new int[]{nr, nc});
+                                
+                            }
+                        }
+                    }
                 }
             }
         }
-        
-        return result;
+        return count;
     }
-
-    public void dfs(char[][] grid, int row, int col, int m, int n){
-            if(row < 0 || row >= m || col < 0 || col >= n || grid[row][col] != '1'){
-                return;
-            } 
-            grid[row][col] = '0';
-            int[][] dirs = new int[][] {{0,1}, {1, 0},{0, -1},{-1, 0}};
-            for(int[] dir : dirs){
-                int nr = dir[0] + row;
-                int nc = dir[1] + col;
-                dfs(grid, nr, nc, m, n);
-            }
-        }
 }
 
 //BFS 
-// Time Complexity: O(m *n)
-// Space Complexity: O(min(m*n))
+// Time Complexity: O(2(M *N))
+// Space Complexity: O(Min(M,N))
