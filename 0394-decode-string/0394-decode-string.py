8965bdc883e5804034092e5c6290565795cbb1f9
Sanket Bagul <sbagul077@gmail.com>
2026-03-04 20:27:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (89.74%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,43 +1,31 @@
 class Solution:
-    i = 0
     def decodeString(self, s: str) -> str:
-        
-        numSt = []
-        strSt = []
-        # strSt.append("")
-        curr = [""]
-        i = 0
-        num = 0
-        # result = ""
+        if len(s) == 0:
+            return ""
 
-        for i in range(len(s)):
-            char = s[i]
-            # i += 1
+        # numSt = []
+        # charSt = []
+        nums = 0
+        result = [""]
+        
+        for char in s:
+
             if char.isdigit():
-                num = num * 10 + int(char)
+                nums = nums * 10 + (int(char))
+
             elif char == "[":
-                # print(num)
-                numSt.append(num)
-                strSt.append("".join(curr))
-                curr = [""]
-                num = 0
-                # print(numSt, strSt)
+                result.append(nums)
+                result.append("")
+                nums = 0
+
             elif char == "]":
-                newStr = []
-                times = numSt.pop()
-                # print(times)
-                for i in range(times):
-                    newStr.append("".join(curr))  
-                curr = [strSt.pop() + "".join(newStr)]
-            else:
-                curr.append(char)
+                # print(numSt,charSt,i)
+                strSt = result.pop()
+                times = result.pop()
             
-            i += 1
-
-        
-        return "".join(curr)
-
-
-
-
+                result[-1] += strSt * times
+            else:
+                result[-1] += char
+        # print(result)
+        return "".join(result)
 
