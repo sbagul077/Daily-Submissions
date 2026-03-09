b9005afccaf2d14a0122f3b166fda06758f9bcc7
Sanket Bagul <sbagul077@gmail.com>
2026-03-09 16:43:21 +0530

    Time: 3 ms (76.61%), Space: 19.5 MB (9.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -3,37 +3,43 @@ class Solution:
         if board is None or len(board) == 0:
             return False
         
-        #row
-        for i in range(9):
-            row = [False for i in range(9)]
-            for j in range(9):
+        # rows
+        for i in range(len(board)):
+            rows = [False for row in range(9)]
+            for j in range(len(board[0])):
                 if board[i][j] != ".":
-                    if row[int(board[i][j]) - 1]:
+                    if rows[int(board[i][j]) - 1] == True:
                         return False
-                    
-                    row[int(board[i][j]) - 1] = True
+                    rows[int(board[i][j]) - 1] = True
         
-        #column        
-        for j in range(9):
-            col = [False for j in range(9)]
-            for i in range(9):
-                if board[i][j] != ".":
-                    if col[int(board[i][j]) - 1]:
-                        return False
-                    col[int(board[i][j]) - 1] = True
+        #Column check
 
-        #block
+        for j in range(len(board[0])):
+            cols = [False for col in range(9)]
+            for i in range(len(board)):
+                if board[i][j] != ".":
+                    if cols[int(board[i][j]) - 1] == True:
+                        return False
+                    cols[int(board[i][j]) - 1] = True
+        
+
+        #Block check
+
         for b in range(9):
-            block = [False for i in range(9)]
-            for i in range((b//3) * 3, ((b // 3) * 3) + 3):
-                for j in range((b % 3) * 3, ((b % 3) * 3) + 3):
+            blocks = [False for block in range(9)]
+            for i in range(b // 3 * 3, b // 3 * 3 + 3):
+                for j in range(b % 3 * 3, b % 3 * 3 + 3):
                     if board[i][j] != ".":
-                        if block[int(board[i][j]) - 1]:
+                        if blocks[int(board[i][j]) - 1] == True:
                             return False
-                        block[int(board[i][j]) - 1] = True
-        
-        return True
-        
+                        blocks[int(board[i][j]) - 1] = True
 
-#Time Complexity: O(n)
-#Space Complexity: O(1)
+        return True
+
+# //Time Complexity: O(1) - 9x9 grid means constant time.
+# //Space Complexity: O(1) - Only 27 sets used (constant space), each holds up to 9 elements.
+# Iterate rows, cols to check for duplicates and maintain a array boolean array of length 9
+# for blocks since its a 3 x 3 block. if we take division for block number and divide by 3 and then multiple by 3 we get the exact row of the current block and take mod for columns
+
+# row = (b / 3 * 3) - (b / 3 * 3 + 3)
+# col = (b % 3 * 3) - (b % 3 * 3 + 3)
