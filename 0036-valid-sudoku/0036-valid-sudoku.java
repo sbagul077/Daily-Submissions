25b8107a6260c094da5401992f4d0e8ec1d9d2fd
Sanket Bagul <sbagul077@gmail.com>
2026-03-09 16:34:35 +0530

    Time: 1 ms (99.86%), Space: 46.4 MB (81.44%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,51 +1,59 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        if(board == null || board.length == 0){
+        if(board ==  null || board.length == 0){
             return false;
-        }
+        }        
 
-        for(int i = 0; i < 9; i++){
-            boolean[] row = new boolean[9];
-            for(int j = 0; j < 9; j++){
-                if(board[i][j] != '.'){
-                    // System.out.println(board[i][j]);
-                    // System.out.println(row[board[i][j] - '0' - 1]);
-                    if((row[board[i][j]- '0' - 1]) == true){
+        //row
+        for(int i = 0; i < board.length; i++){
+            boolean[] numbers = new boolean[9];
+            for(int j = 0; j < board[0].length; j++){
+                if (board[i][j] != '.'){
+
+                    if(numbers[board[i][j] - '1'] == true){
+                        // System.out.println("Failed in rows");
                         return false;
                     }
-                    row[board[i][j]- '0' - 1] = true;
+                    numbers[board[i][j] - '1'] = true;
                 }
             }
         }
 
-        for(int j = 0; j < 9; j++){
-            boolean[] col = new boolean[9];
-            for(int i= 0; i < 9; i++){
+        //column
+        for(int j = 0; j < board[0].length; j++){
+            boolean[] numbers = new boolean[9];
+            for(int i = 0; i< board.length; i++){
                 if(board[i][j] != '.'){
-                    // System.out.println(board[i][j] - '0' - 1);
-                    // System.out.println(col[board[i][j] - '0' - 1]);
-                    if (col[board[i][j] - '0' - 1] == true){
+                    if(numbers[board[i][j] - '1'] == true){
+
                         return false;
                     }
-                    col[board[i][j] - '0' - 1] = true;
+                    numbers[board[i][j] - '1'] = true;
                 }
             }
         }
 
+        //blocks
         for(int b = 0; b < 9; b++){
-            boolean[] li = new boolean[9];
-            for(int i = (b / 3 * 3); i < (b/ 3 * 3 + 3); i++){
-                for(int j = b % 3 * 3; j < b % 3 *3 + 3; j++){
+            boolean[] numbers = new boolean[9];
+            for(int i = b /3 * 3; i < (b / 3 * 3 + 3); i++){
+                for(int j = b % 3 * 3; j <(b % 3 * 3 + 3); j++){
                     if(board[i][j] != '.'){
-                        if(li[board[i][j] - '1']){
+
+
+                        if(numbers[board[i][j] - '1'] == true){
+
                             return false;
                         }
-                        li[board[i][j] - '0' - 1] = true;
+                        numbers[board[i][j] - '1'] = true;
                     }
                 }
             }
         }
-        return true;
-
+    
+    return true;
     }
 }
+
+//Time Complexity: O(1) - 9x9 grid means constant time.
+//Space Complexity: O(1) - Only 27 sets used (constant space), each holds up to 9 elements.
