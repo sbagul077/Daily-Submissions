930286060b20475868a94f6682e3ce837ee548ea
Sanket Bagul <sbagul077@gmail.com>
2026-03-14 15:16:52 +0530

    Time: 143 ms (9.68%), Space: 52.7 MB (68.75%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -3,46 +3,39 @@ class Solution {
     public List<String> addOperators(String num, int target) {
         result = new ArrayList<>();
 
-        helper(num, target, 0, 0, 0, new StringBuilder());
+        helper(num, target, 0, 0, 0, "");
         return result;
     }
 
-    private void helper(String num, int target, int index, long calc, long tail, StringBuilder path){
-        if(index == num.length()){
-            if(target == calc){
-                result.add(path.toString());
+    private void helper(String num, int target, int index, long calc, long tail, String path){
+        //base
+        if(index ==  num.length()){
+            if(calc == target){
+                result.add(path);
             }
-        return;        
+            return;
         }
-
+        //logic
         for(int i = index; i < num.length(); i++){
+            //preceding zero
             if(i != index && num.charAt(index) == '0'){
                 continue;
             }
-            long curr = Long.parseLong(num.substring(index, i + 1));
-            int le = path.toString().length();
+            long curr = Long.parseLong(num.substring(index, i + 1)); 
             if(index == 0){
-                path.append(curr); // action
-                helper(num, target, i + 1, curr, curr, path); //recurse
-                path.setLength(le);
-            }
-            else{
-                //+
-                path.append("+");//action
-                path.append(curr);//action
-                helper(num, target, i + 1, calc + curr, curr, path); // recurse
-                path.setLength(le); //backtrack
-                // -
-                path.append("-"); //action
-                path.append(curr); //action
-                helper(num, target, i + 1, calc - curr, -curr, path); //recurse
-                path.setLength(le); //backtrack
-                //*
-                path.append("*"); // action
-                path.append(curr); //action
-                helper(num, target, i + 1, calc - tail + (tail * curr), tail * curr, path); // recurse
-                path.setLength(le); //backtrack
+                helper(num, target, i + 1, curr, curr, path + curr);
+            } else {
+                // +
+                helper(num, target, i + 1, calc + curr, curr, path + '+' + curr);
+                // - 
+                helper(num, target, i + 1, calc - curr, -curr, path + '-' + curr);
+                // *
+                helper(num, target, i + 1, calc - tail + (tail * curr), tail * curr, path + '*' + curr);
             }
         }
     }
 }
+
+// Recursive 
+// Time Complexity: O(2^n)
+// Space complexity: O(2^n)
