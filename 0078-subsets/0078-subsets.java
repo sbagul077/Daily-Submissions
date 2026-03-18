0ef731426f0e7b5e5d66c7d2a3f52ddbdc7aec85
Sanket Bagul <sbagul077@gmail.com>
2026-03-18 21:34:55 +0530

    Time: 1 ms (88.2%), Space: 44.2 MB (71.5%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,25 +1,26 @@
 class Solution {
-    List<List<Integer>> result;
+    
     public List<List<Integer>> subsets(int[] nums) {
-        result = new ArrayList<>();
-        // if(nums == null || nums.length == 0){
-        //     return result;
-        // }
-        helper(nums, 0, new ArrayList<>());
-        return result;
-    }
-
-    private void helper(int[] nums, int index, List<Integer> path){
-        result.add(new ArrayList<>(path));
-
-        for(int i = index; i < nums.length; i++){
-            path.add(nums[i]);
-            helper(nums, i + 1, path);
-            path.remove(path.size() - 1);
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length == 0){
+            return result;
         }
+
+        result.add(new ArrayList<>());
+
+        for(int i = 0; i < nums.length; i++){
+            int size = result.size();
+            for(int k = 0; k < size; k++){
+                List<Integer> temp = new ArrayList<>(result.get(k));
+                temp.add(nums[i]);
+                result.add(temp);
+            }
+        }
+
+        return result;
     }
 }
 
-// #For loop Recursion with Backtracking
-// #TC : O(2^n)
-// #SC :O(2^n)
+// For Loop
+// Time Complexity: O(n^2)
+// Space Complexity: O(h)
