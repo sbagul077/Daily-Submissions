7aec336a7a5d8383f7f29dae7e698966c5b6299b
Sanket Bagul <sbagul077@gmail.com>
2026-03-22 23:33:26 +0530

    Time: 3931 ms (50.52%), Space: 19.2 MB (97.53%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,38 +1,47 @@
 class Solution:
+    m = 0
+    n = 0
+    dirs = []
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m = len(board)
-        n = len(board[0])
-        dirs = [[0, 1], [0, -1], [-1, 0], [1, 0]]
-
-        def dfs(word, board, row, col, index):
-            #base
-            if row < 0 or col < 0 or row == m or col == n or board[row][col] == "#":
-                return False
-            
-            if len(word) == index:
-                return True
-            
-            if word[index] == board[row][col]:
-                if index == len(word) - 1:
-                    return True
-                
-                ch = board[row][col]
-                board[row][col] = "#"
-
-                for d in dirs:
-                    nr = d[0] + row
-                    nc = d[1] + col
-                    if dfs(word, board, nr, nc, index + 1):
-                        return True
-                
-                board[row][col] = ch
-        
+        if len(board) == 0:
             return False
+        
+        self.dirs = [[0, 1], [0, -1], [1, 0],[-1, 0]]
+        self.m = len(board)
+        self.n = len(board[0])
 
-        for i in range(m):
-            for j in range(n):
-                if dfs(word, board, i, j, 0):
+        for i in range(self.m):
+            for j in range(self.n):
+
+                if self.backtrack(board, word, i, j, 0):
                     return True
-                            
+        
         return False
-                    
+
+    def backtrack(self, board: List[List[str]], word: str, i: int, j: int, index: int) -> bool:
+        #base case
+        if i < 0 or j < 0 or i == self.m or j == self.n or board[i][j] == "#":
+            return False
+        
+        if index == len(word):
+            return True
+
+        if(word[index] == board[i][j]):
+            if index == len(word) - 1:
+                return True
+            char = board[i][j]
+            board[i][j] = "#"
+
+            for d in self.dirs:
+                nr = d[0] + i
+                nc = d[1] + j
+
+                if self.backtrack(board, word, nr, nc, index + 1):
+                    return True
+                
+            board[i][j] = char
+        return False
+
+# For Loop Recursion Backtracking
+# Time Complexity: O(M*N*4^L). Where L is the lenght of the word
+# Space Complexity: O(L). Where L is the lenght of the word
