c28eba95d5e57152902af2ad4f44ea432ccad7b5
Sanket Bagul <sbagul077@gmail.com>
2026-03-25 23:36:03 +0530

    Time: 86 ms (7.48%), Space: 20.6 MB (13.29%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -20,7 +20,7 @@ class Solution:
         for word in words:
             self.insert(word, self.root)
         
-        self.dfs(self.root, [])
+        self.bfs(self.root)
 
         return self.result
 
@@ -36,18 +36,26 @@ class Solution:
         
         curr.isEnd = True
     
-    def dfs(self, curr, path):
-        if len(path) >= len(self.result):
-            self.result = "".join(path)
-        
-        for i in range(25, -1, -1):
-            if curr.children[i] and curr.children[i].isEnd:
-                le = len(path)
-                #action
-                path.append(chr(i + ord("a")))
-                self.dfs(curr.children[i], path)
-                path = path[: le]
-        
-  
+    def bfs(self, curr):
+        currStr = ""
+        queue = deque()
+        strQ = deque()
+
+        queue.append(curr)
+        strQ.append(currStr)
+
+        while queue:
+            curr = queue.popleft()
+            currStr = strQ.popleft()
+
+            for i in range(25, -1, -1):
+                if curr.children[i] and curr.children[i].isEnd:
+                    queue.append(curr.children[i])
+                    strQ.append(currStr + chr(i + ord("a")))
+
+            self.result = currStr
+
+
+#BFS
 #Time Complexity:; O(N * L), N is number of words, L is average word length.
 #Space Compelxity: O(N * L)
