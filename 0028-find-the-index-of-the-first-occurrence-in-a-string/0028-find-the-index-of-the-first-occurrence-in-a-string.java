8566476303ac07ca379b1012f74820570ffb32d7
Sanket Bagul <sbagul077@gmail.com>
2026-05-22 00:20:07 +0530

    Time: 3 ms (9.5%), Space: 42.9 MB (81.81%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,12 +1,12 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (needle.length() == 0)
+        if(needle.length() == 0){
             return 0;
-
+        }
         int m = haystack.length();
         int n = needle.length();
-        int i = 0; //source
-        int j = 0; //pattern
+        int i = 0;
+        int j = 0;
 
         int[] lps = lps(needle);
 
@@ -14,40 +14,43 @@ class Solution {
             if(haystack.charAt(i) == needle.charAt(j)){
                 i += 1;
                 j += 1;
-               
                 if(j == n){
                     return i - n;
                 }
             }
             else if(haystack.charAt(i) != needle.charAt(j) && j > 0){
-                j = lps[j - 1];
-            }else if(haystack.charAt(i) != needle.charAt(j) && j == 0){
-                i++;
+                j = lps[j- 1];
+            }
+            else if(haystack.charAt(i) != needle.charAt(j) && j == 0){
+                i += 1;
             }
         }
 
-        return -1;
+        return -1;        
     }
 
     private int[] lps(String needle){
         int[] lps = new int[needle.length()];
-        lps[0] = 0;
         int i = 1;
         int j = 0;
 
         while(i < needle.length()){
             if(needle.charAt(i) == needle.charAt(j)){
-                j +=1;
+                j += 1;
                 lps[i] = j;
-                i++;
+                i += 1;
             }
             else if(needle.charAt(i) != needle.charAt(j) && j > 0){
                 j = lps[j - 1];
-            }else if(needle.charAt(i) != needle.charAt(j) && j == 0){
-                lps[i] = 0;
-                i++;
+            }
+            else if(needle.charAt(i) != needle.charAt(j) && j == 0){
+                i+= 1;
             }
         }
         return lps;
     }
 }
+
+// KMP Algorithm
+// Time Complexity: O(m + n)
+// Space Complexity: O(n)
