bb24f808c007fc6275f7f6781451be049298c330
Sanket Bagul <sbagul077@gmail.com>
2026-05-22 00:21:07 +0530

    Time: 11 ms (5.15%), Space: 19.9 MB (8.7%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -2,26 +2,49 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if len(needle) == 0:
             return 0
+
         m = len(haystack)
         n = len(needle)
 
-        i = 0
-        # print("this")
-        while i <= m - n:
-            if haystack[i] == needle[0]:
-                # print("this")
-                j = 0
-                k = i
-                while j < n and k < m and haystack[k] == needle[j]:
-                    j += 1
-                    k += 1
+        i = 0 #source
+        j = 0 #pattern
+        lps = self.lps(needle)
 
-                    if j == n:
-                        return i
-            
-            i += 1
+        while i < m:
+            print(i,j)
+            if haystack[i] == needle[j]:
+                # print("this")
+                i += 1
+                j += 1                
+                if j == n:
+                    return i - n
+            elif haystack[i] != needle[j] and j > 0:
+                j = lps[j-1]
+                
+            elif haystack[i] != needle[j] and j == 0:
+                i += 1
         
         return -1
+    
+    def lps(self, needle):
+        lps = [0] * len(needle)
+        # lps = [0 for i in range(len(needle))]
+        i = 1
+        j = 0
+        while(i < len(needle)):
+            if needle[i] == needle[j]:
+                j+= 1
+                lps[i] = j
+                i+= 1
+            elif needle[i] != needle[j] and j > 0:
+                j = lps[j- 1]
+            else:
+                lps[i] = 0
+                i += 1
+            
+        return lps
 
-# Time Complexity: O(m * n)
-# Space Complexity: O(1)
+
+#KMP Algorithm
+#Time Complexity: O(m + n)
+#Space Complexity: O(n)
