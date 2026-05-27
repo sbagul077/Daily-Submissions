89bf808fd9bafc0bd11c0132d72aead94ee1000d
Sanket Bagul <sbagul077@gmail.com>
2026-05-27 23:47:30 +0530

    Time: 0 ms (100%), Space: 19.4 MB (63.72%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.py
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -1,40 +1,35 @@
 class Solution:
-    tens = []
-    below_20 = []
-    # result = []
+    denominations = ["","Hundred", "Thousand", "Million","Billion", "Trillion"]
+    tens = ["Zero", "Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+    below_twenty = ["Zero","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten","Eleven",    
+    "Twelve",  "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+    
     def numberToWords(self, num: int) -> str:
-        self.tens = ["","Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty","Seventy", "Eighty", "Ninety"]
-        self.below_20 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", 
-        "Thirteen", "Fourteen","Fifteen", "Sixteen", "Seventeen","Eighteen", "Nineteen"]
-        result = ""
-        i = 0
-        thousands = [" ", "Thousand ", "Million ", "Billion ", "Trillion "]
-
         if num == 0:
             return "Zero"
-
+        self.denominations = [" ", "Thousand ", "Million ","Billion ", "Trillion "]
+        self.tens = ["Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        self.below_twenty = ["Zero","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten","Eleven",    
+    "Twelve",  "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        result = ""
+        denomination = 0
         while num > 0:
-            # num = num // 1000
             if num % 1000 > 0:
-                result = self.helper(num % 1000) + thousands[i] + result
-            i += 1
+                temp = self.helper(num % 1000) 
+                print(temp, "temp")
+                result = temp +  self.denominations[denomination] + result
+                print(result, "result")
+            denomination += 1
             num = num // 1000
         
         return result.strip()
     
-    def helper(self, num):
-
+    def helper(self, num : int) -> str:
         if num == 0:
             return ""
-        
         if num < 20:
-            return self.below_20[num] + " "
-
-        elif num < 100:
-            # print(num // 10, num % 10)
+            return self.below_twenty[num] + " "
+        elif(num < 100):
             return self.tens[num // 10] + " " + self.helper(num % 10)
         else:
-            # print(num // 100, num % 100)
-            return self.below_20[num // 100] + " Hundred " + self.helper(num % 100)
-
-        
+            return self.below_twenty[num // 100] + " Hundred " + self.helper(num % 100)
