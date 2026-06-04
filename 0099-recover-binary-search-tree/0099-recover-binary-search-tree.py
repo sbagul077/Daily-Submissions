813fa830efe2fb450b4d5142692ea5d89127f8ac
Sanket Bagul <sbagul077@gmail.com>
2026-06-05 00:35:52 +0530

    Time: 1 ms (71.75%), Space: 19.5 MB (94.18%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
@@ -13,26 +13,27 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
+        if root is None:
+            return root
+        
         self.inorder(root)
-        # print(self.first, self.second)
         temp = self.first.val
         self.first.val = self.second.val
         self.second.val = temp
-
+    
     def inorder(self, root):
-        #base
         if root is None:
             return 
         
-        # logic
         self.inorder(root.left)
-        if self.prev is not None and self.prev.val > root.val:
-            if not self.flag:
-                self.first= self.prev
+        #st.pop()
+        if(self.prev != None and self.prev.val >= root.val):
+            if(self.flag == False):
+                self.first = self.prev
                 self.second = root
                 self.flag = True
             else:
                 self.second = root
+        
         self.prev = root
-        # print(self.prev.val)
         self.inorder(root.right)
