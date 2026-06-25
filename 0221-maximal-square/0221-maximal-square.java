cc34558aef6f1f2eb1fc2bd042c5351f57c033b2
Sanket Bagul <sbagul077@gmail.com>
2026-06-25 22:39:36 +0530

    Time: 7 ms (94.65%), Space: 79.3 MB (5.4%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -3,43 +3,33 @@ class Solution {
         if(matrix == null || matrix.length == 0){
             return 0;
         }
-        int max = Integer.MIN_VALUE;
+
         int m = matrix.length;
         int n = matrix[0].length;
-        boolean flag = false;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(matrix[i][j] == '1'){
-                    flag = true;
-                    int curr = 1;
-                    // i + curr ,  j + curr
-                    while(i + curr < m && j + curr < n && flag){
-                        //same row of i + curr and j + curr
-                        for(int k = j; k <= j + curr; k++){
-                            if(matrix[i + curr][k] == '0'){
-                                flag = false;
-                                break;
-                            }
-                        }
+        int max = 0;
+        int prev = 0;
+        int diagUp = 0;
+        int[] dp = new int[n + 1];
 
-                        //same col of i+ curr and j + curr
-                        for(int k = i; k <= i + curr; k++){
-                            if(matrix[k][j + curr] == '0'){
-                                flag = false;
-                                break;
-                            }
-                        }
-                        if(flag == true){
-                            curr++;
-                        }                        
-                    }
-                    max = Math.max(max, curr);
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                prev = dp[j];
+                if(matrix[i-1][j-1] == '1'){
+                    // dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i-1][j], dp[i-1][j-1]));
+                    dp[j] = 1 + Math.min(dp[j-1], Math.min(dp[j], diagUp));
+                    max = Math.max(max, dp[j]);
                 }
+                else{
+                    dp[j] = 0;
+                }
+                diagUp = prev;
             }
         }
-        return max* max;
+
+        return max * max;
     }
 }
-//brute force
-//time complexity :O(m*n)^2
-//Space Complexity: O(1)
+
+// 1D array bottom up DP
+// Time Complexity: O(m *n)
+// Space Complexity: O(n)
