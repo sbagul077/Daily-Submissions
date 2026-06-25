48533aa26d5dc05898e124e5603ca4ba7fd78a5b
Sanket Bagul <sbagul077@gmail.com>
2026-06-25 21:00:47 +0530

    Time: 86 ms (54.33%), Space: 33.2 MB (47.2%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -3,23 +3,21 @@ class Solution:
         if matrix is None or len(matrix) == 0:
             return 0
         
-        m = len(matrix) #row
-        n = len(matrix[0]) #cols
-        result = float("-inf")
-
+        m = len(matrix)
+        n = len(matrix[0])
+        result = 0
         dp = [[0 for i in range(n + 1)] for j in range(m + 1)]
 
-        for i in range(1, len(dp)):
-            for j in range(1, len(dp[0])):
-                if matrix[i - 1][j - 1] == "1":
-                    dp[i][j] = 1 + min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1]))
-                
-                result = max(result, dp[i][j])
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if matrix[i-1][j-1] == "1":
+                    dp[i][j] = 1 + min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]))
+
+                    result = max(result, dp[i][j])
         
         return result * result
-    
-#DP 
-#Time Complexity: O(m * n)
-#Space Complexity: O(m * n)
 
 
+#         // bottom up DP
+# // Time Complexity: O(m *n)
+# // Space Complexity: O(m * n)
