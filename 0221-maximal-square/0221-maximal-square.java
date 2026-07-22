9622b8ff5115b35b9fa1a224d133f4dc7e23e6f4
Sanket Bagul <sbagul077@gmail.com>
2026-07-22 20:58:31 +0530

    Time: 11 ms (17%), Space: 72.3 MB (16.36%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -4,32 +4,29 @@ class Solution {
             return 0;
         }
 
+        int result = 0;
         int m = matrix.length;
         int n = matrix[0].length;
-        int max = 0;
-        int prev = 0;
-        int diagUp = 0;
-        int[] dp = new int[n + 1];
-
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                prev = dp[j];
+        
+        int[][]dp = new int[m+1][n+1];
+        
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[0].length; j++){
                 if(matrix[i-1][j-1] == '1'){
-                    // dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i-1][j], dp[i-1][j-1]));
-                    dp[j] = 1 + Math.min(dp[j-1], Math.min(dp[j], diagUp));
-                    max = Math.max(max, dp[j]);
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                    result = Math.max(dp[i][j], result);
                 }
                 else{
-                    dp[j] = 0;
+                    dp[i][j] = 0;
                 }
-                diagUp = prev;
             }
         }
-
-        return max * max;
+        System.out.println(Arrays.toString(dp));
+        return result * result;
     }
 }
 
-// 1D array bottom up DP
-// Time Complexity: O(m *n)
-// Space Complexity: O(n)
+
+//Brute Force
+// Time Complexity: O(m*n)^2
+// Space Complexity: O(1)
