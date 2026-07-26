02b8c30932de847074ae940705f746024e548d3a
Sanket Bagul <sbagul077@gmail.com>
2026-07-26 23:09:58 +0530

    Time: 183 ms (46.51%), Space: 19.3 MB (86.43%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -7,16 +7,25 @@ class Solution:
         dp[0] = arr[0]
 
         for i in range(1, len(dp)):
-            maximum = dp[i]
-            for j in range(1, k+ 1):
+            maxEle = dp[i]
+
+            for j in range(1, k + 1):
                 if i - j + 1 < 0:
                     break
                 
-                maximum = max(maximum, arr[i - j + 1])
+                maxEle = max(maxEle, arr[i - j + 1])
+
                 if i - j >= 0:
-                    dp[i] = max(dp[i],maximum * j + dp[i-j])
+                    dp[i] = max(dp[i], maxEle *  j + dp[i-j])
                 else:
-                    dp[i] = max(dp[i], maximum * j)
+                    dp[i] = max(dp[i], maxEle * j)
         
-        return dp[len(arr) - 1]
-            
+
+        
+        # return dp[len(arr) - 1]
+        
+        return dp[-1]
+
+# // DP
+# // #time complexity: O(n * k)
+# // Space Complexity: O(n)
