d5543bd337df9e73d921d95bc1a1154a7dbaf037
Sanket Bagul <sbagul077@gmail.com>
2026-09-02 00:49:49 +0530

    Time: 0 ms (100%), Space: 19.4 MB (26.38%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,36 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        i = n - 2
 
-        # [3,6,4,7,5,3,2]
+        n = len(nums)
+        i = n - 2 
         while i >= 0:
             if nums[i] < nums[i + 1]:
+                for j in range(n-1, i, -1):
+                    
+                    if nums[j] > nums[i]:
+                        nums[i], nums[j] = nums[j], nums[i]
+                        # print(nums)
+                        break
+                
                 break
             i -= 1
-        # print(i)
-        j = n - 1
-        if i >= 0:
-            while j > i and nums[j] <= nums[i]:
-                j -= 1
-            print(j)
-            self.swap(nums, i, j)    
-        self.reverse(nums, i + 1)
+        
+        self.reverse(nums, i + 1, n - 1)            
     
-
-    def swap(self, nums, i, j):
-        temp = nums[i]
-        nums[i] = nums[j]
-        nums[j] = temp
+    def reverse(self,nums, start, end):
+        
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
     
-    def reverse(self, nums, left):
-        right = len(nums) - 1
-
-        while left <= right:
-            self.swap(nums, left, right)
-            left += 1
-            right -= 1
-
-
-
